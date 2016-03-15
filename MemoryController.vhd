a3022151e09e44743baa73a2d47378ebfe9e89d6
Raúl Peñacoba <raul.mikaop.zelda@gmail.com>
2016-03-15 20:18:45 +0100

    add templates for memory support

---
+++ MemoryController.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity MemoryController is
+    port (CLOCK_50  : in  std_logic;
+	      addr      : in  std_logic_vector(15 downto 0);
+          wr_data   : in  std_logic_vector(15 downto 0);
+          rd_data   : out std_logic_vector(15 downto 0);
+          we        : in  std_logic;
+          byte_m    : in  std_logic;
+          -- señales para la placa de desarrollo
+          SRAM_ADDR : out   std_logic_vector(17 downto 0);
+          SRAM_DQ   : inout std_logic_vector(15 downto 0);
+          SRAM_UB_N : out   std_logic;
+          SRAM_LB_N : out   std_logic;
+          SRAM_CE_N : out   std_logic := '1';
+          SRAM_OE_N : out   std_logic := '1';
+          SRAM_WE_N : out   std_logic := '1');
+end MemoryController;
+
+architecture comportament of MemoryController is
+begin
+
+end comportament;
